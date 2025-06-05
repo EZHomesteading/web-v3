5c2fe6c5510e6ad8507ceebc771175cf1600aa07
zachmshort <zach.short@fantomworks.com>
2025-06-04 21:18:51 -0400

    deleting unused files

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,5 +1,5 @@
-import { handlePaymentIntentAmountCapturable } from "@/utils/webhook-handlers/payment-intent-amount-capturable-update";
-import { webhookProcessor } from "@/utils/webhook-handlers/webhook-processor";
+import { handlePaymentIntentAmountCapturable } from "@/utils/stripe-webhook-handlers/payment-intent-amount-capturable-updated";
+import { webhookProcessor } from "@/utils/stripe-webhook-handlers/webhook-processor";
 import { NextRequest } from "next/server";
 import Stripe from "stripe";
 
@@ -11,6 +11,7 @@ const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
 export async function POST(request: NextRequest) {
   return webhookProcessor(request, stripe, endpointSecret, {
-    "payment_intent.amount_capturable_updated": handlePaymentIntentAmountCapturable,
+    "payment_intent.amount_capturable_updated":
+      handlePaymentIntentAmountCapturable,
   });
 }
